d79d091d8f64e77082fddf2f084abd8a081a42b2
lamali <lamali292@gmail.com>
2026-04-14 02:09:11 +0200

    Refactored stance logic to use `WatcherStanceModel` as the base class. Removed `StancePower`, simplified VFX, and updated corresponding stance, relic, power, and potion logic for consistency.

--- Code/Powers/MantraPower.cs
+++ Code/Powers/MantraPower.cs
@@ -1,12 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using BaseLib.Abstracts;
 using BaseLib.Extensions;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Models;
 using Watcher.Code.Abstract;
 using Watcher.Code.Commands;
+using Watcher.Code.Core;
 using Watcher.Code.Extensions;
 using Watcher.Code.Stances;
 using Watcher.Code.Stances.Vfx;
@@ -21,7 +25,7 @@ public sealed class MantraPower : WatcherPowerModel
 
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
     [
-        HoverTipFactory.FromPower<DivinityStance>()
+        WatcherHoverTipFactory.FromStance<DivinityStance>()
     ];
 
     public override async Task AfterPowerAmountChanged(
